26f6c2fadc1d760a2f55a733026711c96094034f
ZokiPet <Sony@Sony-VAIO>
2018-02-02 03:06:09 +0100

    Debuging and some cosmetic changes ToDo: Class methods optimization, Error handling method implementation, moce JSON response from Class method (silly) to decoderHandler file (where it belongs)

--- lib/classDecoder.php
+++ lib/classDecoder.php
@@ -7,107 +7,87 @@
  */
 
 class decoder {
-var $number;
 
-	function __construct($msisdn) {
-		$this->number = $msisdn;
-	}
+    var $number;
+
+    function __construct($msisdn) {
+        $this->number = $msisdn;
+    }
+
+    function set_number($new_msisdn) {        
+        $this->number = $new_msisdn;
+    }
+
+    function get_number() {
+        return $this->number;
+    }
+
+    function sanitize_number() {
+        $this->set_number(preg_replace("/[^0-9,+]/", "", $this->number));
+        $this->set_number(preg_replace("/(^00)/", "", $this->number));
+    }
+
+    function decode_number() {
+
+        $countryName = 'n/a';
+        $countryIsoCode = 'n/a';
+        $countryDialingCode = 'n/a';
+        $subscriberNumber = 'n/a';
+        $mnoNumber = 'n/a';
+        $mnoName = 'n/a';
         
-	function set_number($new_msisdn) {        
-            $this->number = $new_msisdn;
-	}
-        
-	function get_number() {
-            return $this->number;
-	}
+        /* loading CC data from JSON */
+        $url = '../data/cc_codes.json';
+        $data = file_get_contents($url);
+        $cc_codes = json_decode($data);
 
-	function sanitize_number() {
-            return preg_replace("/[^0-9,+]/", "", $this->number);
-	}
+        //$this->set_number($this->sanitize_number());
+        $this->sanitize_number();
+        //echo $this->number;
 
-	function decode_number() {
-            
-            /* loading CC data from JSON */
-            $url = '../data/cc_codes.json';
-            $data = file_get_contents($url);
-            $cc_codes = json_decode($data);
+        /*	Loop through COuntry codes to detect Country and Country ISO code */
+        foreach ($cc_codes as $cc) {
+			
+            $match = "/^".$cc->country_code."/";
 
-            $this->set_number($this->sanitize_number());
-
-            /*	Loop through COuntry codes to detect Country and Country ISO code */
-            foreach ($cc_codes as $cc) {
-/*			echo 'the Number -->'.$this->number . '--';
-                //echo $cc->country_code . ' ---> ';
-                //$match = "^\+?[".$cc->country_code."]{1,14}$^";
-                $match = "/^".$cc->country_code."/";
-                echo $match . ' ----> ';
-*/			
-                $match = "/^".$cc->country_code."/";
-
-                if (preg_match($match, $this->number)) {
-                    $countryDialingCode = $cc->country_code;
-                    $countryIsoCode = $cc->iso_code_2;
-                    $countryName = $cc->country;
-                    break;
-                }
+            if (preg_match($match, $this->number)) {
+                $countryDialingCode = $cc->country_code;
+                $countryIsoCode = $cc->iso_code_2;
+                $countryName = $cc->country;
+                break;
             }
-            /* loading MNO data from PHP file coresponding to country code */
-            //echo "mathed CC: " . $cc->country_code;
-            $mnoFile="../data/carriers/".$countryDialingCode.".php";
-            //echo "MNO file is ---> " . $mnoFile;
-            if (is_file($mnoFile)) {
-                $mnoArray = include $mnoFile;
-            }
-            else { 
-                echo "MNO file is missing !!!";
-                /* HANDLE AN ERROR*/ 
-            }
+        }
+        /* loading MNO data from PHP file coresponding to country code */
+        $mnoFile="../data/carriers/".$countryDialingCode.".php";
+        if (is_file($mnoFile)) {
+            $mnoArray = include $mnoFile;
             /* Loop through MNO code for detected Country Code */
             foreach ($mnoArray as $mnoCode => $value) {
-                //echo "MNO CODE --->" . $mnoCode . $value .'<br/>';
-                //echo "MNO CODE --->" . $mnoCode .'<br/>';
                 $match = "/^".$mnoCode."/";
                 if (preg_match($match, $this->number)) {
-                    //echo 'match --> '.$match . 'number ---> ' . $this->number . ' Provider Name' . $value . '<br/>';
-                    //$countryDialingCode
-                    $mnoName = $value;
-                    $subscriberNumber = preg_replace($match, "", $this->number);
-                    //echo $subscriberNumber;
-                    $mno_number = preg_replace("/^".$countryDialingCode."/", "", $mnoCode);
-
-                    //header('Content-Type: application/json');
-//                    echo "<br/>";
-//                    echo "countryName ---> ". $countryName; echo "<br/>";
-//                    echo "countryIsoCode ---> ". $countryIsoCode; echo "<br/>";
-//                    echo "countryDialingCode ---> ". $countryDialingCode; echo "<br/>";
-//                    echo "subscriberNumber ---> ". $subscriberNumber; echo "<br/>";
-//                    echo "mno_number ---> ". $mno_number; echo "<br/>";
-//                    echo "Operator Name ---> ". $mnoName; echo "<br/>";					
-
-                    break;
-                }
-            }
-            
-//            return array(
-//                "countryName" => $countryName,
-//                "countryIsoCode" => $countryIsoCode,
-//                "countryDialingCode" => $countryDialingCode,
-//                "subscriberNumber" => $subscriberNumber,
-//                "mno_number" => $mno_number,
-//                "Operator Name" => $mnoName
-//            );
-            //$response = [];
-            $response =[$countryName, $countryIsoCode, $countryDialingCode, $subscriberNumber, $mno_number, $mnoName];            
-            header('Content-Type: application/json');
-            echo json_encode($response); 
+                   $mnoName = $value;
+                   $subscriberNumber = preg_replace($match, "", $this->number);
+                   $mnoNumber = preg_replace("/^".$countryDialingCode."/", "", $mnoCode);
+                   break;
+               }
+           }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        else { 
+            //No data file for detected countryDialingCode";
+            /* HANDLE AN ERROR*/ 
+            $mnoName = 'n/a';
+            $subscriberNumber = 'n/a';
+            $mnoNumber = 'n/a';
+        }
+        //$response = [];
+        //$response =[$countryName, $countryIsoCode, $countryDialingCode, $subscriberNumber, $mno_number, $mnoName];
+        $response =["countryName" => $countryName,
+                    "countryIsoCode" => $countryIsoCode,
+                    "countryDialingCode" => $countryDialingCode,
+                    "subscriberNumber" => $subscriberNumber,
+                    "mnoNumber" => $mnoNumber,
+                    "Operator Name" => $mnoName];
+        header('Content-Type: application/json');
+        echo json_encode($response); 
+    }   
 }
--- lib/decoderHandler.php
+++ lib/decoderHandler.php
@@ -8,14 +8,18 @@
 
 require_once('classDecoder.php');
 
-if (isset( $_GET['call_func']) and isset( $_GET['msisdn'])) {
-    if ($_GET['call_func'] === 'decode_msisdn') {
-        $msisdn = $_GET['msisdn'];
+$get_func = filter_input(INPUT_GET, 'call_func');
+//$get_param = filter_input(INPUT_GET, 'msisdn');
+//if (isset( $_GET['call_func']) and isset( $_GET['msisdn'])) {
+    //if ($_GET['call_func'] === 'decode_msisdn') {
+    if ($get_func === 'decode_msisdn') {
+        //$msisdn = $_GET['msisdn'];
+        $msisdn = filter_input(INPUT_GET, 'msisdn');
         //echo "decode_msisdn " . $msisdn;
 
         $msisdnToDecode = new decoder($msisdn);
         $msisdnToDecode->decode_number();
     }
-}
+//}
             
 ?>
